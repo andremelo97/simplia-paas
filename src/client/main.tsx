ed966ddf63d57866c866ad6878d9da24de8c303f
andremelo97 <andre.melo9715@gmail.com>
2025-10-18 00:29:02 -0300

    fix: add i18n and FeedbackHost to Internal-Admin

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { AppRoutes } from './apps/internal-admin/routes/index'
+import { FeedbackHost } from './common/feedback'
+import './common/i18n' // Initialize i18n
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <AppRoutes />
+      <FeedbackHost />
     </BrowserRouter>
   </React.StrictMode>
 )
